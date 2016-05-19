c86acbd548399f27072742a2ad4e3593ffaae180
Thanat01 <thanat.tunti@gmail.com>
2016-05-19 18:01:05 +0700

    bulk image upload

--- Web/OStore/Controllers/ProductController.cs
+++ Web/OStore/Controllers/ProductController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OStore.Models.Product;
+using OStore.Models.ProductImport;
 
 namespace OStore.Controllers
 {
@@ -23,7 +24,7 @@ namespace OStore.Controllers
         [HttpGet, ActionName("List")]
         public ActionResult List(int? categoryId)
         {
-            return View(SessionProvider.Instance.GetProduct(categoryId??0));
+            return View(SessionProvider.Instance.GetProduct(categoryId ?? 0));
         }
 
         [HttpGet]
@@ -64,6 +65,98 @@ namespace OStore.Controllers
                 return Json(new { Message = "Secondary" });
             else
                 return Json(new { Message = "Error in saving file" });
+        }
+
+        [HttpGet]
+        public ActionResult ExcelImport()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ImageBulkUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ImportProductFromExcel()
+        {
+            bool isSavedSuccessfully = true;
+            try
+            {
+                //foreach (string fileName in Request.Files)
+                //{
+                //    HttpPostedFileBase file = Request.Files[fileName];
+                //    if (file != null && file.ContentLength > 0)
+                //    {
+                //        ImageProvider.Instance.SaveProduct(file, Session["ProductId"].ToString(), "");
+                //    }
+                //}
+            }
+            catch
+            {
+                isSavedSuccessfully = false;
+            }
+
+            if (isSavedSuccessfully)
+                return Json(new { Message = "Secondary" });
+            else
+                return Json(new { Message = "Error in saving file" });
+        }
+
+        [HttpPost]
+        public ActionResult BulkImageUpload()
+        {
+            bool isSavedSuccessfully = true;
+            try
+            {
+                //foreach (string fileName in Request.Files)
+                //{
+                //    HttpPostedFileBase file = Request.Files[fileName];
+                //    if (file != null && file.ContentLength > 0)
+                //    {
+                //        ImageProvider.Instance.SaveProduct(file, Session["ProductId"].ToString(), "");
+                //    }
+                //}
+            }
+            catch
+            {
+                isSavedSuccessfully = false;
+            }
+
+            if (isSavedSuccessfully)
+                return Json(new { Message = "Secondary" });
+            else
+                return Json(new { Message = "Error in saving file" });
+        }
+
+
+        [HttpGet]
+        public ActionResult ImageBulkUploadList()
+        {
+
+            List<ImportImageViewModel> models = new List<ImportImageViewModel>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                ImportImageViewModel model = new ImportImageViewModel()
+                {
+                    SKU = "SKU000" + i.ToString(),
+                    Name = "Dress",
+                    Description = "Simply dummy text of the printing and typesetting industry",
+                    IsChecked = false,
+                    IsActive = false
+                };
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    model.Images.Add(string.Format("{0}/products/{1}{2}.jpg", SessionProvider.Instance.CurrentShop.Id, i, j));
+                }
+                models.Add(model);
+            }
+
+            return View(models);
         }
     }
 }
---
+++ Web/OStore/Models/ProductImport/ImportImageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OStore.Models.ProductImport
+{
+    public class ImportImageViewModel
+    {
+        public ImportImageViewModel()
+        {
+            Images = new List<string>();
+        }
+
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool IsChecked { get; set; }
+        public bool IsActive { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
